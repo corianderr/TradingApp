aa67666ed7c30dfd19bf4cdd8865a409f5397d21
Karina Andriushchenko <karina.andr@icloud.com>
2023-04-16 17:40:58 +0600

    Create KucoinProvider class and implement logic for GetPriceAsync method

---
+++ Providers/KucoinProvider.cs
@@ -0,0 +1,26 @@
+﻿using Binance.Net.Clients;
+using Kucoin.Net.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingApp.Providers
+{
+    internal class KucoinProvider : ICryptoPriceProvider
+    {
+        private readonly KucoinClient kucoinClient;
+
+        public KucoinProvider()
+        {
+            kucoinClient = new KucoinClient();
+        }
+
+        public async Task<decimal> GetPriceAsync(string symbol)
+        {
+            var priceData = await kucoinClient.SpotApi.ExchangeData.GetTickerAsync(symbol);
+            return priceData.Data.LastPrice == null ? 0 : (decimal)priceData.Data.LastPrice;
+        }
+    }
+}
