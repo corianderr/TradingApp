4d2346827900120a7ae553e367088f5ee67fd8f0
Karina Andriushchenko <karina.andr@icloud.com>
2023-04-16 18:04:04 +0600

    Create BybitProvider class and implement logic for GetPriceAsync method

---
+++ Providers/BybitProvider.cs
@@ -0,0 +1,25 @@
+﻿using Bybit.Net.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingApp.Providers
+{
+    internal class BybitProvider : ICryptoPriceProvider
+    {
+        private readonly BybitClient bybitClient;
+
+        public BybitProvider()
+        {
+            bybitClient = new BybitClient();
+        }
+
+        public async Task<decimal> GetPriceAsync(string symbol)
+        {
+            var priceData = await bybitClient.SpotApiV1.ExchangeData.GetPriceAsync(symbol);
+            return priceData.Data.Price;
+        }
+    }
+}
