af4461779f2e94653bb66b47ff7641e747016a4a
Karina Andriushchenko <karina.andr@icloud.com>
2023-04-16 17:26:20 +0600

    Create BinanceProvider class and implement logic for GetPriceAsync method

---
+++ Providers/BinanceProvider.cs
@@ -0,0 +1,25 @@
+﻿using Binance.Net.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingApp.Providers
+{
+    internal class BinanceProvider : ICryptoPriceProvider
+    {
+        private readonly BinanceClient binanceClient;
+
+        public BinanceProvider()
+        {
+            binanceClient = new BinanceClient();
+        }
+
+        public async Task<decimal> GetPriceAsync(string symbol)
+        {
+            var priceData = await binanceClient.SpotApi.ExchangeData.GetPriceAsync("BTCUSDT");
+            return priceData.Data.Price;
+        }
+    }
+}
